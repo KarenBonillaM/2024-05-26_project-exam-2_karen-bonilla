2535bd99d28a7b25093e64a9fc8c960b904ba993
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-05-08 10:33:14 +0200

    Adds apis URL

--- src/Shared/apis.js
+++ src/Shared/apis.js
@@ -1,3 +1,8 @@
-const API_BASE_URL = "https://v2.api.noroff.dev/";
-export const API_HOLIDAZE = `${API_BASE_URL}holidaze/venues`;
+const API_BASE_URL = "https://v2.api.noroff.dev";
+const API_AUTH = `${API_BASE_URL}/auth`;
+export const API_AUTH_LOGIN = `${API_AUTH}/login`;
+export const API_AUTH_REGISTER = `${API_AUTH}/register`;
+export const API_HOLIDAZE = `${API_BASE_URL}/holidaze`;
+export const API_HOLIDAZE_VENUES = `${API_HOLIDAZE}/venues`;
+export const API_HOLIDAZE_PROFILES = `${API_HOLIDAZE}/profiles`;
 
