c46453785255a434ef764556eae27dfb6d55d417
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-05-24 18:50:21 +0200

    Made profile component interactive

--- src/Components/Profile/index.jsx
+++ src/Components/Profile/index.jsx
@@ -6,6 +6,7 @@ import { API_HOLIDAZE_PROFILES } from "../../Shared/apis";
 
 function Profile() {
   let { name } = useParams();
+  console.log("Name:", name);
   const { data, isLoading, isError } = useFetchSingle(
     `${API_HOLIDAZE_PROFILES}/${name}`
   );
@@ -23,12 +24,24 @@ function Profile() {
     return <div>No profile data available</div>;
   }
   return (
-    <div>
-      <img src={data.avatar?.url} alt={data.avatar?.alt}></img>
-      <h2>{data.name}</h2>
-      <p>{data.email}</p>
-      <div>{data.venueManager ? <p>Manager</p> : <p>User</p>}</div>
-      <div>
+    <div className="">
+      <figure className="pt-6 pb-0">
+        <span className="relative inline-flex h-24 w-24 items-center justify-center rounded-full text-white">
+          <img
+            src={data.avatar?.url}
+            alt={data.avatar?.alt}
+            className="w-24 h-24 max-w-full rounded-full"></img>
+        </span>
+      </figure>
+      <div className="p-6">
+        <header className="mb-1">
+          <h2 className="text-xl font-medium text-slate-700">{data.name}</h2>
+          <div className=" text-slate-400">
+            {data.venueManager ? <div>Manager</div> : <div>User</div>}
+          </div>
+        </header>
+      </div>
+      <div className="text-left p-3 pt-0">
         <p>Bookings: {data._count.bookings}</p>
         <p>Venues: {data._count.venues}</p>
       </div>
