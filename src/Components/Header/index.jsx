6caee46acc2b456bc07753efdf600ba43d25d714
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-05-24 18:47:12 +0200

    Adds styling on header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -2,7 +2,8 @@ import Nav from "../Nav";
 
 function Header() {
   return (
-    <header>
+    // <header className="px-8 py-2 border-b-1 relative z-20 w-full border-b border-slate-200 bg-white/90 shadow-lg shadow-slate-700/5 after:absolute after:left-0 after:top-full after:z-10 after:block after:h-px after:w-full after:bg-slate-200 lg:border-slate-200 lg:backdrop-blur-sm lg:after:hidden">
+    <header className="border-b-1 relative z-20 w-full border-b border-slate-200 bg-white/90 shadow-lg shadow-slate-700/5 after:absolute after:left-0 after:top-full after:z-10 after:block after:h-px after:w-full after:bg-slate-200 lg:border-slate-200 lg:backdrop-blur-sm lg:after:hidden">
       <Nav />
     </header>
   );
