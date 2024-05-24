9d295aa29ee3574040adba0492f05303c0558337
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-05-24 18:59:24 +0200

    Adds carousel on venues images

--- src/Components/Venue/index.jsx
+++ src/Components/Venue/index.jsx
@@ -1,14 +1,29 @@
-import { useFetchSingle } from "../../Hooks/useFetchSingle";
+import useFetchVenue from "../../Hooks/useFetchVenue";
+import Slider from "react-slick";
 import { useParams } from "react-router-dom";
 import { API_HOLIDAZE_VENUES } from "../../Shared/apis";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faWifi,
+  faCar,
+  faUtensils,
+  faPaw,
+  faStar,
+  faCheck,
+  faLocationDot,
+} from "@fortawesome/free-solid-svg-icons";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import House from "../../images/house.jpg";
+import BookVenueForm from "../../Components/Forms/BookVenue";
 
 function Venue() {
   let { id } = useParams();
-  const { data, isLoading, isError } = useFetchSingle(
+  const { venue, isLoading, isError } = useFetchVenue(
     `${API_HOLIDAZE_VENUES}/${id}`
   );
 
-  if (isLoading || !data) {
+  if (isLoading || !venue) {
     return <div>Loading...</div>;
   }
 
@@ -16,20 +31,231 @@ function Venue() {
     return <div>Something went wrong...</div>;
   }
 
-  console.log(data);
+  console.log(venue);
+
+  const SliderArrow = ({ className, style, onClick, position }) => {
+    const arrowStyle =
+      position === "next"
+        ? { ...style, right: "10px" }
+        : { ...style, left: "10px", zIndex: "1" };
+
+    return <div className={className} style={arrowStyle} onClick={onClick} />;
+  };
+
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <SliderArrow position="next" />,
+    prevArrow: <SliderArrow position="prev" />,
+  };
 
   return (
-    <div>
-      <div key={data.id}>
-        <div>id: {data.name}</div>
-        <img
-          className="w-2/4"
-          src={data.media[0].url}
-          alt={data.media[0].alt}></img>
-        <div>Price ${data.price}</div>
-        <div>Rating {data.rating}</div>
-        <p>{data.description}</p>
-      </div>
+    <div
+      key={venue.id}
+      className="venue-container container m-auto py-6 lg:px-20">
+      <section className="section-image-venue">
+        <div className="venue-image-container">
+          {venue.media && venue.media.length > 1 ? (
+            <Slider {...sliderSettings}>
+              {venue.media.map((media, index) => (
+                <div key={index}>
+                  <img
+                    className="h-96 w-full object-fill rounded-lg"
+                    src={media.url}
+                    alt={media.alt || "venue image"}></img>
+                </div>
+              ))}
+            </Slider>
+          ) : (
+            <img
+              src={
+                venue.media && venue.media.length === 1
+                  ? venue.media[0].url
+                  : House
+              }
+              alt={
+                venue.media && venue.media.length === 1
+                  ? venue.media[0].alt
+                  : "house"
+              }
+              className="
+              h-96
+              w-full
+              object-fill
+              rounded-lg"></img>
+          )}
+        </div>
+      </section>
+      <section className="section-venue-booking grid-cols-4 gap-6 md:grid-cols-8 lg:grid-cols-12 pt-8 sm:grid">
+        <div className="venue-info-container col-span-4 lg:col-span-7">
+          <div>
+            <h1 className="text-3xl py-4">{venue.name}</h1>
+            <ul className="divide-y divide-slate-100 border-b-2 border-red">
+              <li class="flex items-start gap-4 px-4 py-3">
+                <div className="flex flex-col gap-0 min-h-[2rem] items-start justify-center">
+                  <p className="text-base text-slate-700 ">
+                    Price: {venue.price} SEK
+                  </p>
+                </div>
+              </li>
+              <li className="flex items-start gap-4 px-4 py-3">
+                <FontAwesomeIcon icon={faStar} />
+                <div class="flex flex-col gap-0 min-h-[2rem] items-start justify-center text-base text-slate-700">
+                  {venue.rating}
+                </div>
+              </li>
+              <li className="flex items-start gap-4 px-4 py-3">
+                <div className="flex flex-col gap-0 min-h-[2rem] items-start justify-center">
+                  <div class="text-base text-slate-700">
+                    Max guests: {venue.maxGuests}
+                  </div>
+                </div>
+              </li>
+              <li className="flex items-start gap-4 px-4 py-3">
+                <div className="flex flex-col gap-0 min-h-[2rem] items-start justify-center">
+                  <p class="text-base text-slate-700">{venue.description}</p>
+                </div>
+              </li>
+              <li className="flex items-start gap-4 px-4 py-3">
+                <FontAwesomeIcon icon={faLocationDot} />
+                <div class="flex flex-col gap-0 min-h-[2rem] items-start justify-center text-base text-slate-700">
+                  {venue.location.address}
+                </div>
+                <div className="flex flex-col gap-0 min-h-[2rem] items-start justify-center text-base text-slate-700">
+                  {venue.location.city}
+                </div>
+                <div className="flex flex-col gap-0 min-h-[2rem] items-start justify-center text-base text-slate-700">
+                  {venue.location.country}
+                </div>
+              </li>
+              <li className="flex items-start gap-4 px-4 py-3">
+                <div className="flex flex-col gap-0 min-h-[2rem] items-start justify-center">
+                  <div className="text-base text-slate-700">
+                    Bookings: {venue._count.bookings}
+                  </div>
+                </div>
+              </li>
+            </ul>
+
+            <ul className="divide-y divide-slate-100 pt-8">
+              <h2 className="text-xl pb-3">Amenities</h2>
+              <li className="flex items-center gap-4 px-4 py-3">
+                <div className="flex items-center self-center text-blue-500">
+                  <FontAwesomeIcon className="h-6 w-6" icon={faWifi} />
+                </div>
+                <div className="flex min-h-[2rem] flex-1 flex-col items-start justify-center gap-0 overflow-hidden">
+                  <label
+                    className="w-full cursor-pointer truncate text-base text-slate-700"
+                    htmlFor="meta.wifi">
+                    Wifi
+                  </label>
+                </div>
+                <div className="flex items-center self-center">
+                  <div className="relative flex flex-wrap items-center">
+                    <input
+                      className="peer h-4 w-4 cursor-pointer appearance-none rounded border-2 border-slate-500 bg-white transition-colors checked:border-blue-500 checked:bg-blue-500 checked:hover:border-blue-600 checked:hover:bg-blue-600 focus:outline-none checked:focus:border-blue-700 checked:focus:bg-blue-700 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-slate-100 disabled:bg-slate-50"
+                      type="checkbox"
+                      checked={venue.meta.wifi}
+                      readOnly
+                    />
+                    <FontAwesomeIcon
+                      className="text-white pointer-events-none absolute top-0 left-0 h-4 w-4 -rotate-90 fill-white stroke-white opacity-0 transition-all duration-300 peer-checked:rotate-0 peer-checked:opacity-100 peer-disabled:cursor-not-allowed"
+                      icon={faCheck}
+                    />
+                  </div>
+                </div>
+              </li>
+              <li className="flex items-center gap-4 px-4 py-3">
+                <div className="flex items-center self-center text-blue-500">
+                  <FontAwesomeIcon className="h-6 w-6" icon={faCar} />
+                </div>
+                <div className="flex min-h-[2rem] flex-1 flex-col items-start justify-center gap-0 overflow-hidden">
+                  <label
+                    className="w-full cursor-pointer truncate text-base text-slate-700"
+                    htmlFor="meta.parking">
+                    Parking
+                  </label>
+                </div>
+                <div className="flex items-center self-center">
+                  <div className="relative flex flex-wrap items-center">
+                    <input
+                      className="peer h-4 w-4 cursor-pointer appearance-none rounded border-2 border-slate-500 bg-white transition-colors checked:border-blue-500 checked:bg-blue-500 checked:hover:border-blue-600 checked:hover:bg-blue-600 focus:outline-none checked:focus:border-blue-700 checked:focus:bg-blue-700 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-slate-100 disabled:bg-slate-50"
+                      type="checkbox"
+                      checked={venue.meta.parking}
+                      readOnly
+                    />
+                    <FontAwesomeIcon
+                      className="text-white pointer-events-none absolute top-0 left-0 h-4 w-4 -rotate-90 fill-white stroke-white opacity-0 transition-all duration-300 peer-checked:rotate-0 peer-checked:opacity-100 peer-disabled:cursor-not-allowed"
+                      icon={faCheck}
+                    />
+                  </div>
+                </div>
+              </li>
+
+              <li className="flex items-center gap-4 px-4 py-3">
+                <div className="flex items-center self-center text-blue-500">
+                  <FontAwesomeIcon className="h-6 w-6" icon={faUtensils} />
+                </div>
+                <div className="flex min-h-[2rem] flex-1 flex-col items-start justify-center gap-0 overflow-hidden">
+                  <label
+                    className="w-full cursor-pointer truncate text-base text-slate-700"
+                    htmlFor="meta.breakfast">
+                    Breakfast
+                  </label>
+                </div>
+                <div className="flex items-center self-center">
+                  <div className="relative flex flex-wrap items-center">
+                    <input
+                      className="peer h-4 w-4 cursor-pointer appearance-none rounded border-2 border-slate-500 bg-white transition-colors checked:border-blue-500 checked:bg-blue-500 checked:hover:border-blue-600 checked:hover:bg-blue-600 focus:outline-none checked:focus:border-blue-700 checked:focus:bg-blue-700 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-slate-100 disabled:bg-slate-50"
+                      type="checkbox"
+                      checked={venue.meta.breakfast}
+                      readOnly
+                    />
+                    <FontAwesomeIcon
+                      className="text-white pointer-events-none absolute top-0 left-0 h-4 w-4 -rotate-90 fill-white stroke-white opacity-0 transition-all duration-300 peer-checked:rotate-0 peer-checked:opacity-100 peer-disabled:cursor-not-allowed"
+                      icon={faCheck}
+                    />
+                  </div>
+                </div>
+              </li>
+              <li className="flex items-center gap-4 px-4 py-3">
+                <div className="flex items-center self-center text-blue-500">
+                  <FontAwesomeIcon className="h-6 w-6" icon={faPaw} />
+                </div>
+                <div className="flex min-h-[2rem] flex-1 flex-col items-start justify-center gap-0 overflow-hidden">
+                  <label
+                    className="w-full cursor-pointer truncate text-base text-slate-700"
+                    htmlFor="meta.pets">
+                    Pets
+                  </label>
+                </div>
+                <div className="flex items-center self-center">
+                  <div className="relative flex flex-wrap items-center">
+                    <input
+                      className="peer h-4 w-4 cursor-pointer appearance-none rounded border-2 border-slate-500 bg-white transition-colors checked:border-blue-500 checked:bg-blue-500 checked:hover:border-blue-600 checked:hover:bg-blue-600 focus:outline-none checked:focus:border-blue-700 checked:focus:bg-blue-700 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-slate-100 disabled:bg-slate-50"
+                      type="checkbox"
+                      checked={venue.meta.pets}
+                      readOnly
+                    />
+                    <FontAwesomeIcon
+                      className="text-white pointer-events-none absolute top-0 left-0 h-4 w-4 -rotate-90 fill-white stroke-white opacity-0 transition-all duration-300 peer-checked:rotate-0 peer-checked:opacity-100 peer-disabled:cursor-not-allowed"
+                      icon={faCheck}
+                    />
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <section className="venue-booking-calendar col-span-4 lg:col-span-5 justify-self-center lg:justify-self-end">
+          <div>
+            <BookVenueForm />
+          </div>
+        </section>
+      </section>
     </div>
   );
 }
--- src/Components/Venues/index.jsx
+++ src/Components/Venues/index.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
-import useFetch from "../../Hooks/useFetch";
+import Slider from "react-slick";
+import useFetch from "../../Hooks/useFetchArray";
 import { API_HOLIDAZE_VENUES } from "../../Shared/apis";
-import "./index.css";
 import SearchBar from "../SearchBar";
 import { Link } from "react-router-dom";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import House from "../../images/house.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWifi, faUtensils, faPaw } from "@fortawesome/free-solid-svg-icons";
 
 function Venues() {
   const { venues } = useFetch(`${API_HOLIDAZE_VENUES}`);
@@ -21,36 +26,98 @@ function Venues() {
 
   const venuesToDisplay = searchResults.length > 0 ? searchResults : venues;
 
+  const SliderArrow = ({ className, style, onClick, position }) => {
+    const arrowStyle =
+      position === "next"
+        ? { ...style, right: "10px" }
+        : { ...style, left: "10px", zIndex: "1" };
+
+    return <div className={className} style={arrowStyle} onClick={onClick} />;
+  };
+
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidersToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <SliderArrow position="next" />,
+    prevArrow: <SliderArrow position="prev" />,
+  };
+
   return (
     <div>
-      <div>
-        <SearchBar onSearch={handleSearch} />
-      </div>
-      <div className="grid grid-cols-4 gap-6">
+      <SearchBar onSearch={handleSearch} />
+      <section className="home-page-bio  my-8 m-auto mx-5">
+        <div className="grid grid-cols-4 gap-6 md:grid-cols-8 lg:grid-cols-12 p-4 px-5 py-8 bg-blue-500 text-white h-fit rounded-xl">
+          <div className="col-span-4 lg:col-span-3 text-xl self-center">
+            <h1>Welcome to Holidaze</h1>
+            <p>Find your perfect holiday destination</p>
+          </div>
+          <div className="col-span-4 lg:col-span-3 flex items-center bg-blue-800 rounded-lg px-5 bg-opacity-70 py-8">
+            <FontAwesomeIcon className="pr-5 text-4xl" icon={faUtensils} />
+            <p>Find beautiful places with breakfast included</p>
+          </div>
+          <div className="col-span-4 lg:col-span-3 flex items-center bg-blue-800 rounded-lg px-5 bg-opacity-80 py-8">
+            <FontAwesomeIcon className="pr-5 text-4xl" icon={faPaw} />
+            <p>Have a blast with your best friend</p>
+          </div>
+          <div className="col-span-4 lg:col-span-3 flex items-center bg-blue-800 rounded-lg px-5 bg-opacity-80 py-8">
+            <FontAwesomeIcon className="pr-5 text-4xl" icon={faWifi} />
+            <p>Work remotely in a paradise</p>
+          </div>
+        </div>
+      </section>
+      <h2 className="p-5 font-medium text-2xl">
+        Book your perfect place in your favorite destination
+      </h2>
+      <div className="grid grid-cols-4 gap-6 p-4 md:grid-cols-8 lg:grid-cols-12">
         {venuesToDisplay.map((venue) => (
           <div
             key={venue.id}
-            className="card-venue overflow-hidden rounded bg-white text-slate-500 shadow-md shadow-slate-200">
-            <img
-              src={
-                venue.media && venue.media.length > 0 ? venue.media[0].url : ""
-              }
-              alt={
-                venue.media && venue.media.length > 0 ? venue.media[0].alt : ""
-              }
-              className="aspect-video w-full"
-            />
+            className="relative card-venue overflow-hidden rounded bg-white text-slate-500 shadow-md shadow-slate-200 col-span-4 lg:col-span-3">
+            {venue.media && venue.media.length > 1 ? (
+              <Slider {...sliderSettings}>
+                {venue.media.map((image, index) => (
+                  <div key={index}>
+                    <img
+                      src={image.url}
+                      alt={image.alt || "venue image"}
+                      className="aspect-video w-full h-52"
+                    />
+                  </div>
+                ))}
+              </Slider>
+            ) : (
+              <img
+                src={
+                  venue.media && venue.media.length === 1
+                    ? venue.media[0].url
+                    : House
+                }
+                alt={
+                  venue.media && venue.media.length === 1
+                    ? venue.media[0].alt
+                    : "house"
+                }
+                className="aspect-video w-full h-52"
+              />
+            )}
             <div className="p-6">
               <div className="mb-4">
                 <h2 className="text-xl font-medium text-slate-700">
                   {venue.name}
                 </h2>
-                <p className="text-slate-400">${venue.price}</p>
+                <p className="text-slate-400">
+                  Price: {venue.price} kr SEK night
+                </p>
+                <div>{venue.location.city}</div>
+                <div>{venue.location.country}</div>
               </div>
             </div>
             <div className="flex justify-end p-6 pt-0">
               <Link to={`/venue/${venue.id}`}>
-                <button className="inline-flex h-10 w-full items-center justify-center gap-2 whitespace-nowrap rounded bg-blue-800 px-5 text-sm font-medium tracking-wide text-white transition duration-300 hover:bg-blue-700 focus:bg-emerald-700 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none">
+                <button className="absolute bottom-4 right-4 w-30 inline-flex h-10 items-center justify-center gap-2 whitespace-nowrap rounded bg-blue-800 px-5 text-sm font-medium tracking-wide text-white transition duration-300 hover:bg-blue-600 focus:bg-emerald-700 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none">
                   <span>Book now!</span>
                 </button>
               </Link>
