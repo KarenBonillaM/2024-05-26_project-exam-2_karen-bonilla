d8ff00f0bcd00f8cca3f5fecd3b1e266f90a1999
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-05-24 18:54:36 +0200

    Adds styling on search bar component

--- src/Components/SearchBar/index.jsx
+++ src/Components/SearchBar/index.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import useSearch from "../../Hooks/useSearch";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function SearchBar({ onSearch }) {
   const { query, handleInputChange, handleSubmit } = useSearch({ onSearch });
 
   return (
-    <form onKeyUp={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Search..."
-        value={query}
-        onChange={handleInputChange}
-      />
-      <button type="submit">Search</button>
+    <form onKeyUp={handleSubmit} className="search-bar flex justify-center">
+      <div className="flex justify-between h-14 w-1/2 p-2 border border-gray-300 shadow-md rounded-md shadow-slate-200">
+        <input
+          className="w-full p-2 rounded-l-md border-none focus:outline-none"
+          type="text"
+          placeholder="Search..."
+          value={query}
+          onChange={handleInputChange}
+        />
+        <button
+          type="submit"
+          className="p-1 bg-blue-600 w-10 rounded-full text-white hover:bg-blue-500">
+          <FontAwesomeIcon icon={faSearch} />
+        </button>
+      </div>
     </form>
   );
 }
