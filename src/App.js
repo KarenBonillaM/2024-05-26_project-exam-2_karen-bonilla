7d17e843dee2dd68269d15cef3aa28ea8b501f4e
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-05-24 18:40:36 +0200

    Adds route paths on app

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import HomePage from "./Pages/HomePage";
 import VenuePage from "./Pages/VenuePage";
 import SignUpPage from "./Pages/SignUpPage";
 import LogInPage from "./Pages/LogInPage";
-import UserProfilePage from "./Pages/UserProfilePage";
+import UserPage from "./Pages/UserPage";
 import NewVenuePage from "./Pages/NewVenuePage";
 import EditVenuePage from "./Pages/EditVenuePage";
+import FeaturesPage from "./Pages/FeaturesPage";
+import ContactPage from "./Pages/ContactPage";
+import AboutUsPage from "./Pages/AboutUsPage";
 
 function App() {
   return (
@@ -18,7 +21,10 @@ function App() {
           <Route path="venue/:id" element={<VenuePage />} />
           <Route path="signup" element={<SignUpPage />} />
           <Route path="login" element={<LogInPage />} />
-          <Route path="userprofile/:name" element={<UserProfilePage />} />
+          <Route path="contact" element={<ContactPage />} />
+          <Route path="about" element={<AboutUsPage />} />
+          <Route path="features" element={<FeaturesPage />} />
+          <Route path="user/:name" element={<UserPage />} />
           <Route path="newVenue" element={< NewVenuePage />} />
           <Route path="editVenue/:id" element={< EditVenuePage />} />
        </Route>
