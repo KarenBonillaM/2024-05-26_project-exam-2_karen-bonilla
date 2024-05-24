fb15734a648543d6202319a2e0a1d3e408470d26
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-05-24 18:45:51 +0200

    Adds contact, about and feature pages

---
+++ src/Pages/AboutUsPage/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function AboutUsPage() {
+  return (
+    <main className="about-us body-page p-8">
+      <h1 className="text-3xl font-bold text-center text-slate-700 mb-9">
+        About Us
+      </h1>
+      <section className="about-us-content m-28">
+        <p className="text-slate-400">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </p>
+      </section>
+    </main>
+  );
+}
+
+export default AboutUsPage;
