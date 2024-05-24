e41696f361dc6e302669d16df516433c93c82349
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-05-24 19:00:04 +0200

    Adds fetching hooks

---
+++ src/Hooks/useFetchArray/index.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+function useFetch(url) {
+  const [venues, setVenues] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  console.log(url);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+        setVenues(json.data);
+
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+        setIsLoading(false);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchData();
+  }, [url]);
+
+  return { venues, isLoading, isError };
+}
+
+export default useFetch;
---
+++ src/Hooks/useFetchSingle/index.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { load } from "../../Shared/storage.js";
+
+export function useFetchSingle(url) {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  // let { name } = useParams();
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        setIsLoading(true);
+        setIsError(false);
+
+        const token = await load("token");
+        console.log("Token:", token);
+        if (!token) {
+          setIsError("token not found");
+          return;
+        }
+
+        const response = await fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+            "X-Noroff-API-Key": "25333fa4-e617-4cfb-a41d-9fd9a59c9f28",
+          },
+        });
+        const json = await response.json();
+
+        console.log(json);
+
+        setData(json.data);
+      } catch (error) {
+        setIsError(true);
+
+        console.error("Error fetching data:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getData();
+  }, [url]);
+
+  return { data, isLoading, isError };
+}
