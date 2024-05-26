4bfbf4a8e852e0482756d5695e235ad0a44a01d9
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-05-26 15:47:09 +0200

    Adds filters by amenities

--- src/Components/Venues/index.jsx
+++ src/Components/Venues/index.jsx
@@ -8,29 +8,89 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import House from "../../images/house.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faWifi, faUtensils, faPaw } from "@fortawesome/free-solid-svg-icons";
+import {
+  faWifi,
+  faUtensils,
+  faPaw,
+  faCar,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Venues() {
   const { venues } = useFetch(`${API_HOLIDAZE_VENUES}`);
-
-  console.log(venues);
-
   const [searchResults, setSearchResults] = useState([]);
+  const [isWifiFiltered, setIsWifiFiltered] = useState(false);
+  const [isBreakfastFiltered, setIsBreakfastFiltered] = useState(false);
+  const [isPetFriendlyFiltered, setIsPetFriendlyFiltered] = useState(false);
+  const [isParkingFiltered, setIsParkingFiltered] = useState(false);
+
+  const onButtonClickFilterWifi = () => {
+    setIsWifiFiltered(!isWifiFiltered);
+  };
+
+  const onButtonClickFilterBreakfast = () => {
+    setIsBreakfastFiltered(!isBreakfastFiltered);
+  };
+
+  const onButtonClickFilterPetsAllowed = () => {
+    setIsPetFriendlyFiltered(!isPetFriendlyFiltered);
+  };
+
+  const onButtonClickFilterParking = () => {
+    setIsParkingFiltered(!isParkingFiltered);
+  };
 
   const handleSearch = (query) => {
-    const results = venues.filter((venue) =>
-      venue.name.toLowerCase().includes(query.trim().toLowerCase())
-    );
+    const queryLower = query.trim().toLowerCase();
+    const results = venues.filter((venue) => {
+      const name = venue.name ? venue.name.toLowerCase() : "";
+      const city = venue.location.city ? venue.location.city.toLowerCase() : "";
+      const country = venue.location.country
+        ? venue.location.country.toLowerCase()
+        : "";
+
+      return (
+        name.includes(queryLower) ||
+        city.includes(queryLower) ||
+        country.includes(queryLower)
+      );
+    });
     setSearchResults(results);
   };
 
-  const venuesToDisplay = searchResults.length > 0 ? searchResults : venues;
+  const applyFilters = () => {
+    let filteredVenues = venues;
+
+    if (isWifiFiltered) {
+      filteredVenues = filteredVenues.filter(
+        (venue) => venue.meta.wifi === true
+      );
+    }
+    if (isBreakfastFiltered) {
+      filteredVenues = filteredVenues.filter(
+        (venue) => venue.meta.breakfast === true
+      );
+    }
+    if (isPetFriendlyFiltered) {
+      filteredVenues = filteredVenues.filter(
+        (venue) => venue.meta.pets === true
+      );
+    }
+    if (isParkingFiltered) {
+      filteredVenues = filteredVenues.filter(
+        (venue) => venue.meta.parking === true
+      );
+    }
+    return filteredVenues;
+  };
+
+  const venuesToDisplay =
+    searchResults.length > 0 ? searchResults : applyFilters();
 
   const SliderArrow = ({ className, style, onClick, position }) => {
     const arrowStyle =
       position === "next"
-        ? { ...style, right: "10px" }
-        : { ...style, left: "10px", zIndex: "1" };
+        ? { ...style, right: "12px" }
+        : { ...style, left: "8px", zIndex: "1" };
 
     return <div className={className} style={arrowStyle} onClick={onClick} />;
   };
@@ -48,30 +108,55 @@ function Venues() {
   return (
     <div>
       <SearchBar onSearch={handleSearch} />
-      <section className="home-page-bio  my-8 m-auto mx-5">
-        <div className="grid grid-cols-4 gap-6 md:grid-cols-8 lg:grid-cols-12 p-4 px-5 py-8 bg-blue-500 text-white h-fit rounded-xl">
-          <div className="col-span-4 lg:col-span-3 text-xl self-center">
-            <h1>Welcome to Holidaze</h1>
-            <p>Find your perfect holiday destination</p>
-          </div>
-          <div className="col-span-4 lg:col-span-3 flex items-center bg-blue-800 rounded-lg px-5 bg-opacity-70 py-8">
-            <FontAwesomeIcon className="pr-5 text-4xl" icon={faUtensils} />
-            <p>Find beautiful places with breakfast included</p>
-          </div>
-          <div className="col-span-4 lg:col-span-3 flex items-center bg-blue-800 rounded-lg px-5 bg-opacity-80 py-8">
-            <FontAwesomeIcon className="pr-5 text-4xl" icon={faPaw} />
-            <p>Have a blast with your best friend</p>
-          </div>
-          <div className="col-span-4 lg:col-span-3 flex items-center bg-blue-800 rounded-lg px-5 bg-opacity-80 py-8">
-            <FontAwesomeIcon className="pr-5 text-4xl" icon={faWifi} />
-            <p>Work remotely in a paradise</p>
+      <section className="home-page-bio my-8 m-auto mx-5">
+        <div className="p-4 px-5 py-8 bg-blue-500 text-white h-fit rounded-xl">
+          <h2 className="pl-2 pb-2 text-xl text-black font-semibold">
+            Amenities
+          </h2>
+          <div className="grid grid-cols-4 gap-6 md:grid-cols-8 lg:grid-cols-12 ">
+            <button
+              onClick={onButtonClickFilterParking}
+              className={`col-span-4 lg:col-span-3 flex items-center rounded-lg px-5 py-8 cursor-pointer transition-colors duration-300 hover:bg-blue-700 ${
+                isParkingFiltered ? "bg-gray-700" : "bg-blue-800"
+              }`}>
+              <FontAwesomeIcon className="pr-5 text-4xl" icon={faCar} />
+              <p className="text-left">
+                Find beautiful places with breakfast included
+              </p>
+            </button>
+            <button
+              onClick={onButtonClickFilterBreakfast}
+              className={`col-span-4 lg:col-span-3 flex items-center rounded-lg px-5 py-8 cursor-pointer transition-colors duration-300 hover:bg-blue-700 ${
+                isBreakfastFiltered ? "bg-gray-700" : "bg-blue-800"
+              }`}>
+              <FontAwesomeIcon className="pr-5 text-4xl" icon={faUtensils} />
+              <p className="text-left">
+                Find beautiful places with breakfast included
+              </p>
+            </button>
+            <button
+              onClick={onButtonClickFilterPetsAllowed}
+              className={`col-span-4 lg:col-span-3 flex items-center rounded-lg px-5 py-8 cursor-pointer transition-colors duration-300 hover:bg-blue-700 ${
+                isPetFriendlyFiltered ? "bg-gray-700" : "bg-blue-800"
+              }`}>
+              <FontAwesomeIcon className="pr-5 text-4xl" icon={faPaw} />
+              <p className="text-left">Have a blast with your best friend</p>
+            </button>
+            <button
+              onClick={onButtonClickFilterWifi}
+              className={`col-span-4 lg:col-span-3 flex items-center rounded-lg px-5 py-8 cursor-pointer transition-colors duration-300 hover:bg-blue-700 ${
+                isWifiFiltered ? "bg-gray-700" : "bg-blue-800"
+              }`}>
+              <FontAwesomeIcon className="pr-5 text-4xl" icon={faWifi} />
+              <p className="text-left">Work remotely in a paradise</p>
+            </button>
           </div>
         </div>
       </section>
       <h2 className="p-5 font-medium text-2xl">
         Book your perfect place in your favorite destination
       </h2>
-      <div className="grid grid-cols-4 gap-6 p-4 md:grid-cols-8 lg:grid-cols-12">
+      <section className="venues-container grid grid-cols-4 gap-6 p-4 md:grid-cols-8 lg:grid-cols-12">
         {venuesToDisplay.map((venue) => (
           <div
             key={venue.id}
@@ -124,7 +209,7 @@ function Venues() {
             </div>
           </div>
         ))}
-      </div>
+      </section>
     </div>
   );
 }
