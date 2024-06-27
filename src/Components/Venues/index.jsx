e1431c3abb59f1faf02722e0d05de109449c6dea
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-06-27 13:32:09 +0200

    Adds image url validation

--- src/Components/Venues/index.jsx
+++ src/Components/Venues/index.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
 import useFetch from "../../Hooks/useFetchArray";
-import { API_HOLIDAZE_VENUES } from "../../Shared/apis";
+import {
+  API_HOLIDAZE_VENUES,
+  API_HOLIDAZE_VENUES_SEARCH,
+} from "../../Shared/apis";
 import SearchBar from "../SearchBar";
 import { Link } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
@@ -20,73 +23,60 @@ function Venues() {
   const { venues } = useFetch(`${API_HOLIDAZE_VENUES}`);
   const [isLoading, setIsLoading] = useState(false);
   const [searchResults, setSearchResults] = useState([]);
-  const [isWifiFiltered, setIsWifiFiltered] = useState(false);
-  const [isBreakfastFiltered, setIsBreakfastFiltered] = useState(false);
-  const [isPetFriendlyFiltered, setIsPetFriendlyFiltered] = useState(false);
-  const [isParkingFiltered, setIsParkingFiltered] = useState(false);
+  const [filters, setFilters] = useState({
+    isWifiFiltered: false,
+    isBreakfastFiltered: false,
+    isPetFriendlyFiltered: false,
+    isParkingFiltered: false,
+  });
 
-  const onButtonClickFilterWifi = () => {
-    setIsWifiFiltered(!isWifiFiltered);
+  const handleSearch = async (query) => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(`${API_HOLIDAZE_VENUES_SEARCH}?q=${query}`);
+      if (!response.ok) {
+        throw new Error("An error occurred while fetching the data");
+      }
+
+      const json = await response.json();
+      const venuesSearched = json.data;
+
+      if (query === "") {
+        setSearchResults(venues);
+      } else if (venuesSearched.length === 0) {
+        setSearchResults([]);
+      } else {
+        setSearchResults(venuesSearched);
+      }
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+    }
   };
 
-  const onButtonClickFilterBreakfast = () => {
-    setIsBreakfastFiltered(!isBreakfastFiltered);
+  const handleFilterToggle = (filter) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [filter]: !prevFilters[filter],
+    }));
   };
 
-  const onButtonClickFilterPetsAllowed = () => {
-    setIsPetFriendlyFiltered(!isPetFriendlyFiltered);
-  };
-
-  const onButtonClickFilterParking = () => {
-    setIsParkingFiltered(!isParkingFiltered);
-  };
-
-  const handleSearch = (query) => {
-    const queryLower = query.trim().toLowerCase();
-    const results = venues.filter((venue) => {
-      const name = venue.name ? venue.name.toLowerCase() : "";
-      const city = venue.location.city ? venue.location.city.toLowerCase() : "";
-      const country = venue.location.country
-        ? venue.location.country.toLowerCase()
-        : "";
-
+  const applyFilters = (venuesToFilter) => {
+    return venuesToFilter.filter((venue) => {
+      const meta = venue.meta || {};
       return (
-        name.includes(queryLower) ||
-        city.includes(queryLower) ||
-        country.includes(queryLower)
+        (!filters.isWifiFiltered || meta.wifi === true) &&
+        (!filters.isBreakfastFiltered || meta.breakfast === true) &&
+        (!filters.isPetFriendlyFiltered || meta.pets === true) &&
+        (!filters.isParkingFiltered || meta.parking === true)
       );
     });
-    setSearchResults(results);
-  };
-
-  const applyFilters = () => {
-    let filteredVenues = venues;
-
-    if (isWifiFiltered) {
-      filteredVenues = filteredVenues.filter(
-        (venue) => venue.meta.wifi === true
-      );
-    }
-    if (isBreakfastFiltered) {
-      filteredVenues = filteredVenues.filter(
-        (venue) => venue.meta.breakfast === true
-      );
-    }
-    if (isPetFriendlyFiltered) {
-      filteredVenues = filteredVenues.filter(
-        (venue) => venue.meta.pets === true
-      );
-    }
-    if (isParkingFiltered) {
-      filteredVenues = filteredVenues.filter(
-        (venue) => venue.meta.parking === true
-      );
-    }
-    return filteredVenues;
   };
 
   const venuesToDisplay =
-    searchResults.length > 0 ? searchResults : applyFilters();
+    searchResults.length > 0
+      ? applyFilters(searchResults)
+      : applyFilters(venues);
 
   const SliderArrow = ({ className, style, onClick, position }) => {
     const arrowStyle =
@@ -107,26 +97,64 @@ function Venues() {
     prevArrow: <SliderArrow position="prev" />,
   };
 
+  const isValidImageUrl = async (url) => {
+    try {
+      if (!url) return false;
+
+      const response = await fetch(url);
+      if (!response.ok) {
+        return false;
+      }
+
+      const blob = await response.blob();
+      return blob.size > 0;
+    } catch (error) {
+      console.error("Error checking image URL:", error);
+      return false;
+    }
+  };
+
+  const validateImages = async (venues) => {
+    const venuesWithValidImages = await Promise.all(
+      venues.map(async (venue) => {
+        const isValid = await isValidImageUrl(
+          venue.media && venue.media.length === 1 ? venue.media[0].url : null
+        );
+        return {
+          ...venue,
+          isValidImage: isValid,
+        };
+      })
+    );
+
+    return venuesWithValidImages;
+  };
+
   useEffect(() => {
-    setIsLoading(true);
-    setTimeout(() => {
+    const fetchVenues = async () => {
+      setIsLoading(true);
+      const validatedVenues = await validateImages(venues);
+      setSearchResults(validatedVenues);
       setIsLoading(false);
-    }, 2000);
+    };
+
+    fetchVenues();
   }, [venues]);
 
   return (
     <div>
       <SearchBar onSearch={handleSearch} />
       <section className="home-page-bio my-8 m-auto mx-5">
+        {/* Filter buttons */}
         <div className="p-4 px-5 py-8 bg-blue-500 text-white h-fit rounded-xl">
           <h2 className="pl-2 pb-2 text-xl text-black font-semibold">
             Amenities
           </h2>
           <div className="grid grid-cols-4 gap-6 md:grid-cols-8 lg:grid-cols-12 ">
             <button
-              onClick={onButtonClickFilterParking}
+              onClick={() => handleFilterToggle("isParkingFiltered")}
               className={`col-span-4 lg:col-span-3 flex items-center rounded-lg px-5 py-8 cursor-pointer transition-colors duration-300 hover:bg-blue-700 ${
-                isParkingFiltered ? "bg-gray-700" : "bg-blue-800"
+                filters.isParkingFiltered ? "bg-gray-700" : "bg-blue-800"
               }`}>
               <FontAwesomeIcon className="pr-5 text-4xl" icon={faCar} />
               <p className="text-left">
@@ -134,9 +162,9 @@ function Venues() {
               </p>
             </button>
             <button
-              onClick={onButtonClickFilterBreakfast}
+              onClick={() => handleFilterToggle("isBreakfastFiltered")}
               className={`col-span-4 lg:col-span-3 flex items-center rounded-lg px-5 py-8 cursor-pointer transition-colors duration-300 hover:bg-blue-700 ${
-                isBreakfastFiltered ? "bg-gray-700" : "bg-blue-800"
+                filters.isBreakfastFiltered ? "bg-gray-700" : "bg-blue-800"
               }`}>
               <FontAwesomeIcon className="pr-5 text-4xl" icon={faUtensils} />
               <p className="text-left">
@@ -144,17 +172,17 @@ function Venues() {
               </p>
             </button>
             <button
-              onClick={onButtonClickFilterPetsAllowed}
+              onClick={() => handleFilterToggle("isPetFriendlyFiltered")}
               className={`col-span-4 lg:col-span-3 flex items-center rounded-lg px-5 py-8 cursor-pointer transition-colors duration-300 hover:bg-blue-700 ${
-                isPetFriendlyFiltered ? "bg-gray-700" : "bg-blue-800"
+                filters.isPetFriendlyFiltered ? "bg-gray-700" : "bg-blue-800"
               }`}>
               <FontAwesomeIcon className="pr-5 text-4xl" icon={faPaw} />
               <p className="text-left">Have a blast with your best friend</p>
             </button>
             <button
-              onClick={onButtonClickFilterWifi}
+              onClick={() => handleFilterToggle("isWifiFiltered")}
               className={`col-span-4 lg:col-span-3 flex items-center rounded-lg px-5 py-8 cursor-pointer transition-colors duration-300 hover:bg-blue-700 ${
-                isWifiFiltered ? "bg-gray-700" : "bg-blue-800"
+                filters.isWifiFiltered ? "bg-gray-700" : "bg-blue-800"
               }`}>
               <FontAwesomeIcon className="pr-5 text-4xl" icon={faWifi} />
               <p className="text-left">Work remotely in a paradise</p>
@@ -177,58 +205,74 @@ function Venues() {
         </div>
       ) : (
         <section className="venues-container grid grid-cols-4 gap-6 p-4 md:grid-cols-8 lg:grid-cols-12">
-          {venuesToDisplay.map((venue) => (
-            <div
-              key={venue.id}
-              className="relative card-venue overflow-hidden rounded bg-white text-slate-500 shadow-md shadow-slate-200 col-span-4 lg:col-span-3">
-              {venue.media && venue.media.length > 1 ? (
-                <Slider {...sliderSettings}>
-                  {venue.media.map((image, index) => (
-                    <div key={index}>
-                      <img
-                        src={image.url}
-                        alt={image.alt || "venue image"}
-                        className="aspect-video w-full h-52"
-                      />
-                    </div>
-                  ))}
-                </Slider>
-              ) : (
-                <img
-                  src={
-                    venue.media && venue.media.length === 1
-                      ? venue.media[0].url
-                      : House
-                  }
-                  alt={
-                    venue.media && venue.media.length === 1
-                      ? venue.media[0].alt
-                      : "house"
-                  }
-                  className="aspect-video w-full h-52"
-                />
-              )}
-              <div className="p-6">
-                <div className="mb-4">
-                  <h2 className="text-xl font-medium text-slate-700">
-                    {venue.name}
-                  </h2>
-                  <p className="text-slate-400">
-                    Price: {venue.price} kr SEK night
-                  </p>
-                  <div>{venue.location.city}</div>
-                  <div>{venue.location.country}</div>
+          {venuesToDisplay.length === 0 ? (
+            <div className="w-full flex m-auto py-24">
+              <p className="text-2xl">No venues found</p>
+            </div>
+          ) : (
+            venuesToDisplay.map((venue) => (
+              <div
+                key={venue.id}
+                className="relative card-venue overflow-hidden rounded bg-white text-slate-500 shadow-md shadow-slate-200 col-span-4 lg:col-span-3">
+                {venue.media && venue.media.length > 1 ? (
+                  <Slider {...sliderSettings}>
+                    {venue.media.map((image, index) => (
+                      <div key={index}>
+                        {image.url && venue.isValidImage ? (
+                          <img
+                            src={image.url}
+                            alt={image.alt || "venue image"}
+                            className="aspect-video w-full h-52"
+                          />
+                        ) : (
+                          <img
+                            src={House}
+                            alt="house"
+                            className="aspect-video w-full h-52"
+                          />
+                        )}
+                      </div>
+                    ))}
+                  </Slider>
+                ) : (
+                  <img
+                    src={
+                      venue.media &&
+                      venue.media.length === 1 &&
+                      venue.isValidImage
+                        ? venue.media[0].url
+                        : House
+                    }
+                    alt={
+                      venue.media && venue.media.length === 1
+                        ? venue.media[0].alt || "house"
+                        : "house"
+                    }
+                    className="aspect-video w-full h-52"
+                  />
+                )}
+                <div className="p-6">
+                  <div className="mb-4">
+                    <h2 className="text-xl font-medium text-slate-700">
+                      {venue.name}
+                    </h2>
+                    <p className="text-slate-400">
+                      Price: {venue.price} kr SEK night
+                    </p>
+                    <div>{venue.location.city}</div>
+                    <div>{venue.location.country}</div>
+                  </div>
+                </div>
+                <div className="flex justify-end p-6 pt-0">
+                  <Link to={`/venue/${venue.id}`}>
+                    <button className="absolute bottom-4 right-4 w-30 inline-flex h-10 items-center justify-center gap-2 whitespace-nowrap rounded bg-blue-800 px-5 text-sm font-medium tracking-wide text-white transition duration-300 hover:bg-blue-600 focus:bg-emerald-700 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none">
+                      <span>Book now!</span>
+                    </button>
+                  </Link>
                 </div>
               </div>
-              <div className="flex justify-end p-6 pt-0">
-                <Link to={`/venue/${venue.id}`}>
-                  <button className="absolute bottom-4 right-4 w-30 inline-flex h-10 items-center justify-center gap-2 whitespace-nowrap rounded bg-blue-800 px-5 text-sm font-medium tracking-wide text-white transition duration-300 hover:bg-blue-600 focus:bg-emerald-700 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none">
-                    <span>Book now!</span>
-                  </button>
-                </Link>
-              </div>
-            </div>
-          ))}
+            ))
+          )}
         </section>
       )}
     </div>
