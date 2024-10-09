89646437da720ac075b7d781a34b81212ae6231e
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-10-09 15:16:26 +0200

    Adds Pagination component

---
+++ src/Components/Pagination/index.jsx
@@ -0,0 +1,76 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+
+export default function Pagination({ currentPage, totalPages, onPageChange }) {
+  const handlePrev = () => {
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentPage < totalPages) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+      <div className="flex flex-1 justify-between sm:hidden">
+        <button
+          onClick={handlePrev}
+          className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">
+          Previous
+        </button>
+        <button
+          onClick={handleNext}
+          className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">
+          Next
+        </button>
+      </div>
+      <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+        <div>
+          <p className="text-sm text-gray-700">
+            Showing{" "}
+            <span className="font-medium">{(currentPage - 1) * 10 + 1}</span> to{" "}
+            <span className="font-medium">
+              {Math.min(currentPage * 10, totalPages * 10)}
+            </span>{" "}
+            of
+            <span className="font-medium">{totalPages * 10}</span> results
+          </p>
+        </div>
+        <div>
+          <nav
+            aria-label="Pagination"
+            className="isolate inline-flex -space-x-px rounded-md shadow-sm">
+            <button
+              onClick={handlePrev}
+              className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+              <span className="sr-only">Previous</span>
+              <ChevronLeftIcon aria-hidden="true" className="h-5 w-5" />
+            </button>
+            {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
+            {Array.from({ length: totalPages }, (_, index) => (
+              <button
+                key={index + 1}
+                onClick={() => onPageChange(index + 1)}
+                className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0 ${
+                  currentPage === index + 1
+                    ? "bg-indigo-600 text-white"
+                    : "text-gray-900"
+                }`}>
+                {index + 1}
+              </button>
+            ))}
+            <button
+              onClick={handleNext}
+              className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+              <span className="sr-only">Next</span>
+              <ChevronRightIcon aria-hidden="true" className="h-5 w-5" />
+            </button>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+}
